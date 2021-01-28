83f09d284c72d34a14f86937f63de8ad47470622
Fabrice Lamant <flamant@shutterstock.com>
2021-01-28 22:22:04 +0000

    update transaction date on check

--- api/src/api/transaction.ts
+++ api/src/api/transaction.ts
@@ -19,7 +19,13 @@ router.post('/transaction/check', (req, res) => {
       && t.date === date
       && t.name === name);
     if (transaction) {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = (today.getMonth() + 1).toString().padStart(2, '0');
+      const day = today.getDate().toString().padStart(2, '0');
+
       transaction.checked = true;
+      transaction.date = `${year}-${month}-${day}`;
     }
   }
   database.save(dbAccounts);
